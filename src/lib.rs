9aa95a98ae834512ffd5a8bff09257f646e40929
zflcs <1491657576@qq.com>
2024-07-16 21:09:47 +0800

    1. Add pro_macro for `async_main`.

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_attribute]
+pub fn async_main(_args: TokenStream, item: TokenStream) -> TokenStream {
+    let f = syn::parse_macro_input!(item as syn::ItemFn);
+    let attrs  = f.attrs;
+    let block = f.block;
+    quote! {
+        #(#attrs)*
+        fn main() {
+            #[cfg(feature = "axstd")]
+            axstd::futures::block_on(move || async #block);
+        }
+    }.into()
+}
